efb0697e4038f548aa40b7c49ef211cd8bd3b852
jsmal1988 <jsmal1988@gmail.com>
2016-08-20 12:36:21 +0200

    staircase challenge

---
+++ src/challenges/staircase/Staircase.java
@@ -0,0 +1,43 @@
+/**
+ * @author Jakub Smal 
+ * 20.08.2016 12:17:11
+ */
+package challenges.staircase;
+
+import java.util.Scanner;
+
+/*
+ * Input Format
+ * A single integer, , denoting the size of the staircase.
+ * 
+ * Output Format 
+ * Print a staircase of size  using # symbols and spaces. Allignet to right:
+ *   #
+ *  ##
+ * ###  
+ */
+public class Staircase {
+	public static void main(String args[]){
+		 Scanner scanner = new Scanner(System.in);
+		 int number = scanner.nextInt();
+		 
+		 
+		 if(number<=0){
+			 throw new IllegalArgumentException("n has to be positive bigger then 0");
+		 }
+		
+		for(int i = 1; i<=number; i++){
+			int localSpaces = number - i;
+			int localStairs =  i;
+			while(localSpaces>0){
+				System.out.print(" ");
+				localSpaces--;
+			}
+			while(localStairs>0){
+				System.out.print("#");
+				localStairs--;
+			}
+			System.out.println();
+		}
+	}
+}
